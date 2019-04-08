00d4bd6801283978e8678cce19c4af2bfbc98800
Andy <kxie0003@student.monash.edu>
2019-04-08 20:17:00 +1000

    ADD COMMENTS

--- src/main/java/rockets/mining/RocketMiner.java
+++ src/main/java/rockets/mining/RocketMiner.java
@@ -12,7 +12,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//TEST FOR JENKINS
 public class RocketMiner {
     private static Logger logger = LoggerFactory.getLogger(RocketMiner.class);
 
